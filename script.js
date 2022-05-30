6acc7174cb81c27083bddbaf9df39790d501e26b
szkiss04 <zsoltszilardkiss04@gmail.com>
2022-05-30 22:41:23 +0200

    Add play() function; the base game is implemented
    This code is in serious need of refactoring!

--- script.js
+++ script.js
@@ -21,35 +21,55 @@ function playRound( playerSelection, computerSelection ) {
     } else if ( lowerCasePlayerSelection == "rock" ) {
         switch ( computerSelection ) {
             case "paper":
-                return "Paper beats rock, computer wins!";
+                return "Computer wins!";
                 break;
             case "scissors":
-                return "Rock beats scissors, player wins!";
+                return "Player wins!";
                 break;
         }
     } else if ( lowerCasePlayerSelection == "paper" ) {
         switch ( computerSelection ) {
             case "rock":
-                return "Paper beats rock, player wins!";
+                return "Player wins!";
                 break;
             case "scissors":
-                return "Scissors beats paper, computer wins!";
+                return "Computer wins!";
                 break;
         }
     } else if ( lowerCasePlayerSelection == "scissors" ) {
         switch ( computerSelection ) {
             case "rock":
-                return "Rock beats scissors, computer wins!";
+                return "Computer wins!";
                 break;
             case "paper":
-                return "Scissors beats paper, player wins!";
+                return "Player wins!";
                 break;
         }
     }
 }
 
-console.log( playRound( "rock", computerPlay() ) );
-console.log( playRound( "rock", computerPlay() ) );
-console.log( playRound( "rock", computerPlay() ) );
-console.log( playRound( "rock", computerPlay() ) );
-console.log( playRound( "rock", computerPlay() ) );
+function game( ) {
+    let playersChoice, computersChoice, playerPoints = 0, computerPoints = 0, round;
+    for( let i = 0; i < 5; i++ ) {
+        playersChoice = prompt( "What do you choose? Type \"rock\", \"paper\" or \"scissors\" !" );
+        computersChoice = computerPlay( );
+        round = playRound( playersChoice, computersChoice );
+        if ( round == "Player wins!" ) {
+            console.log( "Player's choice " + playersChoice + ", computer's choice: " + computersChoice + ". " + round );
+            playerPoints++;
+        } else if( round == "Computer wins!" ) {
+            console.log( "Player's choice " + playersChoice + ", computer's choice: " + computersChoice + ". " + round );
+            computerPoints++;
+        } else {
+            console.log( round );
+        }
+    }
+    console.log( "Player's points: " + playerPoints + ", computer's points: " + computerPoints );
+    if( playerPoints > computerPoints ) {
+        console.log( "Player won this match! Congrats! :-)" );
+    } else if ( playerPoints < computerPoints ) {
+        console.log( "Computer won this match! It's unfortunate. :-(" );
+    } else {
+        console.log( "The match ended with a tie between you and the computer. Not bad. :-)" );
+    }
+}
